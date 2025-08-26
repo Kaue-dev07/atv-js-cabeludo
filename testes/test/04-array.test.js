90d27134cd9bf65a3cf92652d0977c3c461b77ba
Kauê Rosa <Kaue.dev07@gmail.com>
2025-08-26 10:13:35 -0300

    Update 04-array.test.js

--- testes/test/04-array.test.js
+++ testes/test/04-array.test.js
@@ -2,6 +2,29 @@ const adicionaElemento = require('./04-array')
 
 test(
     'adicione um número a lista', () => {
-        expect(adicionaElemento([1])).toEqual([1])
+        expect(adicionaElemento([1,3],5))
+        .toEqual([1, 3 , 5])
+    }
+)
+
+test(
+    'adicione um  a lista', () => {
+        expect(adicionaElemento(["jp","gv"],"baiano"))
+        .toEqual(["jp", "gv" , "baiano"])
+    }
+)
+
+test(
+    'verifica se tem contem string na lista', () => {
+        const alfabeto = [];
+        const resultado = adicionaElemento(alfabeto, "c")
+        expect(resultado).toContain("c")
+    }
+)
+
+test(
+    'verifica se tem tem uma string igual na lista', () => {
+        expect(adicionaElemento([],"c"))
+        .toEqual([])
     }
 )
